23e2b4f95eba7e2b1af58d2998d5b82a89c49649
CodeLessTraveled <DotNetITPro@Hotmail.com>
2021-08-25 23:39:14 -0700

    First upload to new GitHub repository

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CodeLessTraveled.Osprey
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// CL 20210119
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+   
+            Application.Run(new Form1());
+
+        }
+
+   
+    }
+}
